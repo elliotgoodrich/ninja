4987f7553257b94650fb23420d4e85fdbdbce45b
Elliot Goodrich <elliotgoodrich@gmail.com>
2026-02-04 17:05:48 +0000

    Add more benchmarks for `CanonicalizePath`
    In preparation for performance optimizations on `CanonicalizePath` that
    may affect paths differently, add additional benchmarks on
    
      1. absolute paths
      2. paths with "./"
      3. paths with "../" in the middle
      4. paths with empty directories
      5. paths with backslashes (Windows only)

--- src/canon_perftest.cc
+++ src/canon_perftest.cc
@@ -15,44 +15,129 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "util.h"
+#include <array>
+#include <cassert>
+#include <cstdint>
+#include <limits>
+#include <numeric>
+#include <vector>
+
+#include "string_piece.h"
 #include "metrics.h"
+#include "util.h"
 
-using namespace std;
+const std::string kPaths[] = {
+  // Relative
+  "third_party/WebKit/Source/WebCore/"
+  "platform/leveldb/LevelDBWriteBatch.cpp",
+  // Absolute
+  "/third_party/WebKit/Source/WebCore/"
+  "platform/leveldb/LevelDBWriteBatch.cpp",
+  // Leading ../
+  "../../third_party/WebKit/Source/WebCore/"
+  "platform/leveldb/LevelDBWriteBatch.cpp",
+  // Leading ./ (causing us actually copy bytes)
+  "./third_party/WebKit/Source/WebCore/"
+  "platform/leveldb/LevelDBWriteBatch.cpp",
+  // ../ in the middle
+  "third_party/WebKit/Source/WebCore/"
+  "platform/leveldb/UnnecessarySubDirectory/../LevelDBWriteBatch.cpp",
+  // Empty directories
+  "third_party//WebKit///Source/WebCore/"
+  "platform/leveldb//LevelDBWriteBatch.cpp",
 
-const char kPath[] =
-    "../../third_party/WebKit/Source/WebCore/"
-    "platform/leveldb/LevelDBWriteBatch.cpp";
+#if _WIN32
+  // Handle the same paths again but with backslashes
+  // Relative
+  "third_party\\WebKit\\Source\\WebCore\\"
+  "platform\\leveldb\\LevelDBWriteBatch.cpp",
+  // Absolute
+  "\\third_party\\WebKit\\Source\\WebCore\\"
+  "platform\\leveldb\\LevelDBWriteBatch.cpp",
+  // Leading ..\ dir
+  "..\\..\\third_party\\WebKit\\Source\\WebCore\\"
+  "platform\\leveldb\\LevelDBWriteBatch.cpp",
+  // Leading .\ (causing us actually copy bytes)
+  ".\\third_party\\WebKit\\Source\\WebCore\\"
+  "platform\\leveldb\\LevelDBWriteBatch.cpp",
+  // ..\ in the middle
+  "third_party\\WebKit\\Source\\WebCore\\"
+  "platform\\leveldb\\UnnecessarySubDirectory\\..\\LevelDBWriteBatch.cpp",
+  // Empty directories
+  "third_party\\\\WebKit\\\\\\Source\\WebCore\\"
+  "platform\\leveldb\\\\LevelDBWriteBatch.cpp",
+
+  // Mixture of slashes
+  "third_party\\WebKit/Source\\WebCore\\"
+  "platform\\leveldb/LevelDBWriteBatch.cpp",
+#endif
+};
+
+void disambiguation(char* path, std::size_t* len, std::uint64_t* slash_bits) {
+  // Disambiguate between overloads of CanonicalizePath
+  CanonicalizePath(path, len, slash_bits);
+}
+
+const int kNumRepetitions = 2000000;
+const int kNumRepeats = 5;
+
+template <typename CANONICALIZE_PATH>
+void runBenchmarks(CANONICALIZE_PATH&& f, const char* name,
+                   std::string& pathCopies) {
+  printf("%s\n", name);
+
+  int sum_of_min = 0;
+  int sum_of_max = 0;
+  double sum_of_avg = 0.0;
+
+  for (const std::string& path : kPaths) {
+    printf("%s\n", path.c_str());
+    int min = std::numeric_limits<int>::max();
+    int max = std::numeric_limits<int>::min();
+    double total = 0;
+
+    for (int j = 0; j < kNumRepeats; ++j) {
+      auto out = pathCopies.begin();
+      for (int i = 0; i < kNumRepetitions; ++i) {
+        out = std::copy(path.begin(), path.end(), out);
+      }
+
+      const std::int64_t start = GetTimeMillis();
+      for (int i = 0; i < kNumRepetitions; ++i) {
+        std::uint64_t slash_bits;
+        std::size_t len = path.size();
+        f(&pathCopies[i * len], &len, &slash_bits);
+      }
+
+      const int delta = static_cast<int>(GetTimeMillis() - start);
+      min = std::min(delta, min);
+      max = std::max(delta, max);
+      total += delta;
+    }
+
+    const double avg = total / kNumRepeats;
+    printf("  min %dms  max %dms  avg %.1fms\n", min, max, avg);
+
+    sum_of_min += min;
+    sum_of_max += max;
+    sum_of_avg += avg;
+  }
+
+  printf("AVERAGE:\n  min %dms  max %dms  avg %.1fms\n",
+         static_cast<int>(sum_of_min / std::size(kPaths)),
+         static_cast<int>(sum_of_max / std::size(kPaths)),
+         sum_of_avg / std::size(kPaths));
+}
 
 int main() {
-  vector<int> times;
-
-  char buf[200];
-  size_t len = strlen(kPath);
-  strcpy(buf, kPath);
-
-  for (int j = 0; j < 5; ++j) {
-    const int kNumRepetitions = 2000000;
-    int64_t start = GetTimeMillis();
-    uint64_t slash_bits;
-    for (int i = 0; i < kNumRepetitions; ++i) {
-      CanonicalizePath(buf, &len, &slash_bits);
-    }
-    int delta = (int)(GetTimeMillis() - start);
-    times.push_back(delta);
+  std::size_t max_size = 0;
+  for (const std::string& path : kPaths) {
+    max_size = std::max(max_size, path.size());
   }
 
-  int min = times[0];
-  int max = times[0];
-  float total = 0;
-  for (size_t i = 0; i < times.size(); ++i) {
-    total += times[i];
-    if (times[i] < min)
-      min = times[i];
-    else if (times[i] > max)
-      max = times[i];
-  }
-
-  printf("min %dms  max %dms  avg %.1fms\n",
-         min, max, total / times.size());
+  std::string pathCopies;
+  pathCopies.resize(kNumRepetitions * max_size);
+  runBenchmarks(disambiguation, "CanonicalizePath",
+                pathCopies);
+  // add additional implementations here for comparison
 }
